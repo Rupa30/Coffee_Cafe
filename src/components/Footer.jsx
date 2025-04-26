deb1100de752765f28b02df80e0ad5f0e2a87cb1
Rupa Mahindrakar <rupamahindrakar08@gmail.com>
2025-04-26 20:06:13 +0530

    Change in Icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaFacebookF, FaTwitter, FaInstagram, FaGit, FaGithub, FaLinkedinIn, FaFile, FaFileDownload, FaFilePdf } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaFilePdf } from "react-icons/fa"; 
 
 export default function Footer() {
   return (
@@ -8,13 +8,13 @@ export default function Footer() {
         <p>© {new Date().getFullYear()} Coffee Cafe. All rights reserved.</p>
         <p>Made by Rupa☕</p>
         <div className="flex space-x-6 text-yellow-300">
-          <a href="https://www.linkedin.com/in/rupa-mahindrakar1/" target="_blank" aria-label="Twitter" className="hover:text-yellow-400 transition">
+          <a href="https://www.linkedin.com/in/rupa-mahindrakar1/" target="_blank" aria-label="LinkedIn" className="hover:text-yellow-400 transition">
             <FaLinkedinIn size={20} />
           </a>
-          <a href="https://github.com/Rupa30" target="_blank" aria-label="Instagram" className="hover:text-yellow-400 transition">
+          <a href="https://github.com/Rupa30" target="_blank" aria-label="Github" className="hover:text-yellow-400 transition">
             <FaGithub size={20} />
           </a>
-          <a href="https://drive.google.com/file/d/1XK4gfCe1VbVS7R6UH2oBg-Rx53Khsczt/view?usp=drivesdk" target="_blank" aria-label="Instagram" className="hover:text-yellow-400 transition">
+          <a href="https://drive.google.com/file/d/1XK4gfCe1VbVS7R6UH2oBg-Rx53Khsczt/view?usp=drivesdk" target="_blank" aria-label="ResumeFile" className="hover:text-yellow-400 transition">
             <FaFilePdf size={20} />
           </a>
         </div>
